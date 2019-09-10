1ece589bfa48715308bf11ba8172575b9feff941
Vojtěch Dohnal <vojdoh@gmail.com>
2019-09-10 19:21:49 +0200

    First prerelease, added example

---
+++ examples/simple_get.py
@@ -0,0 +1,15 @@
+import proxpy
+
+my_options = proxpy.Options(proxy_check_threads=200, check_timeout=5, judge=proxpy.Judge.azenv, show_progress=True)
+
+proxpy.open_proxy_file(options=my_options)
+proxpy.check_proxies()
+
+print(f"Alive: {proxpy.proxy_stats['alive']}, Dead: {proxpy.proxy_stats['dead']}")
+print("Proxy list length: ", len(proxpy.proxies))
+
+response = proxpy.pget("https://www.google.com", headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"}, max_retries=20)
+
+print(response.status_code)
+with open("google.html", "w+") as afile:
+    afile.writelines(response.text)
--- proxpy.py
+++ proxpy.py
@@ -4,33 +4,27 @@ from toolz import unique
 import tkinter as tk
 from tkinter import filedialog
 from enum import Enum
-from requests import get, post, session
-from requests.exceptions import RequestException
+from requests import request
+from requests.exceptions import RequestException, ProxyError, Timeout
 from concurrent.futures import ThreadPoolExecutor as ThreadPool
 import re
 from bs4 import BeautifulSoup
 
+# TODO: COMMENTS
+
 root = tk.Tk()
 root.withdraw()
 
+checked = []
 proxies = []
 resetting = False
-debug = False
 proxy_stats = {
     "suc": 0,
     "fail": 0,
+    "alive": 0,
     "dead": 0
 }
-timeout = 6
-check_timeout = 4
-judges = [
-    "https://azenv.net",
-    "http://proxyjudge.info"
-]
-judge = judges[0]
 host_ip = ""
-proxy_check_threads = 50
-checked = []
 
 
 class ProxyType(Enum):
@@ -40,25 +34,95 @@ class ProxyType(Enum):
     socks5 = 3
 
 
+class Judge(Enum):
+    def __new__(cls, *args, **kwargs):
+        value = len(cls.__members__) + 1
+        obj = object.__new__(cls)
+        obj._value_ = value
+        return obj
+
+    def __init__(self, url, number):
+        self.url = url
+        self.number = number
+
+    azenv = "https://azenv.net", 0
+    proxyjudge = "http://proxyjudge.info", 1
+
+
+class Options:
+    def __init__(self, proxy_type=ProxyType.https, username="", password="", judge=Judge.azenv, proxy_check_threads=50,
+                 check_timeout=4,
+                 debug=False,
+                 show_progress=True):
+        self.judge = judge
+        self.proxy_check_threads = proxy_check_threads
+        self.check_timeout = check_timeout
+        self.debug = debug
+        self.proxy_type = proxy_type
+        self.username = username
+        self.password = password
+        self.show_progress = show_progress
+
+
+opts = Options()
+
+
 class Proxy:
     def __init__(self, proxy_address, proxy_type=ProxyType.https.name, user=None, password=None):
         self.proxy = proxy_address
         self.dead = False
         self.banned = False
         self.workers = []
-        if user and password:
-            self.dict_proxy = {
-                "http": f"{proxy_type}://{user}:{password}@{proxy_address}",
-                "https": f"{proxy_type}://{user}:{password}@{proxy_address}",
-            }
-        else:
-            self.dict_proxy = {
-                "http": f"{proxy_type}://{proxy_address}",
-                "https": f"{proxy_type}://{proxy_address}",
-            }
+        self.dict_proxy = {
+            "http": f"{proxy_type}://{user}:{password}@{proxy_address}",
+            "https": f"{proxy_type}://{user}:{password}@{proxy_address}",
+        } if user != "" and password != "" else {
+            "http": f"{proxy_type}://{proxy_address}",
+            "https": f"{proxy_type}://{proxy_address}",
+        }
 
     def __hash__(self):
         return self.proxy.__hash__()
+
+
+def prequest(method, url, **kwargs):
+    if "proxy" not in current_thread().__dict__:
+        get_new_proxy()
+    kwargs["proxies"] = current_thread().__dict__["proxy"].dict_proxy
+    if "session" in kwargs.keys():
+        sess = kwargs["session"]
+        del kwargs["session"]
+    else:
+        sess = None
+    if "max_retries" in kwargs.keys():
+        max_retries = kwargs["max_retries"]
+        del kwargs["max_retries"]
+    else:
+        max_retries = len(proxies)
+    retries = 0
+    while True:
+        try:
+            if sess:
+                response = sess.request(method, url, **kwargs)
+            else:
+                response = request(method, url, **kwargs)
+            return response
+        except (Timeout, ProxyError):
+            get_new_proxy(dead=True)
+            retries += 1
+            if max_retries == retries:
+                raise ConnectionError
+            continue
+
+
+def pget(url, params=None, **kwargs):
+    kwargs.setdefault('allow_redirects', True)
+    return prequest("get", url, params=params, **kwargs)
+
+
+def ppost(url, params=None, **kwargs):
+    kwargs.setdefault('allow_redirects', True)
+    return prequest("get", url, params=params, **kwargs)
 
 
 def choose_from_enum(en, request_text="Please choose from following options:", default_number=0):
@@ -96,19 +160,21 @@ def choose_from_list(lst: list, request_text="Please choose from following optio
 
 
 def get_external_ip(p: Proxy = None):
+    global opts
     try:
         if p:
-            response = get(judge, timeout=check_timeout, proxies=p.dict_proxy)
+            response = request("get", opts.judge.url, timeout=opts.check_timeout, proxies=p.dict_proxy)
         else:
-            print("Getting your IP address, please hold up")
-            response = get(judge, timeout=check_timeout)
+            if __name__ == '__main__':
+                print("Getting your IP address, please hold up")
+            response = request("get", opts.judge.url, timeout=opts.check_timeout)
         soup = BeautifulSoup(response.text, 'html.parser').pre.text
         args = re.split('\n| = ', soup)
         for i in range(len(args)):
             if args[i] == "REMOTE_ADDR":
                 return args[i + 1]
     except RequestException:
-        if debug:
+        if opts.debug:
             print("Please make sure you have access to the internet and try again.")
     return None
 
@@ -129,18 +195,10 @@ def check_proxy(prx: str):
     return False
 
 
-def pretty_print_post(req):
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
-
-
-def get_proxy_thread(dead=False, banned=False):
+def get_new_proxy(dead=False, banned=False):
+    global opts
     if "proxy" in current_thread().__dict__.keys():
-        if debug:
+        if opts.debug:
             print("Proxy down.", current_thread().__dict__["proxy"].proxy)
         if dead:
             current_thread().__dict__["proxy"].dead = True
@@ -153,14 +211,14 @@ def get_proxy_thread(dead=False, banned=False):
 
 
 def test_proxy(prx: Proxy):
-    global judge
+    global opts
     ip = get_external_ip(prx)
     if not ip:
-        if debug:
+        if opts.debug:
             print("Proxy not working.")
     elif ip != host_ip:
         return prx
-    elif debug:
+    elif opts.debug:
         print("Proxy Non-Anonymous, skipping.")
     return False
 
@@ -170,68 +228,68 @@ def proxy_on_checked(future):
     if not future.result():
         proxy_stats["dead"] += 1
     else:
+        proxy_stats["alive"] += 1
         checked.append(future.result())
-    update_proxy_stats(True)
+    if opts.show_progress:
+        update_proxy_stats()
 
 
-def parse_proxies(proxy_file, show_progress, test_proxies):
-    global proxies, proxy_stats, judge, host_ip
-    proxy_type = choose_from_enum(ProxyType, "Please choose type of proxies:", 1).name
-    if test_proxies:
-        judge = choose_from_list(judges, "Please select a proxy judge:")
-        host_ip = get_external_ip()
-    username = input("Please enter username for proxies (blank for no login) -> ")
-    password = None
-    if username != "":
-        password = input("Please enter password for proxies -> ")
-    else:
-        username = None
-    if show_progress:
-        print("--- Starting proxy checking ---")
-        print_proxy_stats(test_proxies)
+def parse_proxies(proxy_file):
+    global proxies, proxy_stats, opts
+    if __name__ == '__main__':
+        opts.proxy_type = choose_from_enum(ProxyType, "Please choose type of proxies:", 1).name
+    if __name__ == '__main__':
+        opts.username = input("Please enter username for proxies (blank for no login) -> ")
+        if opts.username != "":
+            opts.password = input("Please enter password for proxies -> ")
     with open(proxy_file) as p_file:
         for line in p_file.readlines():
             line = line.strip()
             if check_proxy(line):
-                proxies.append(Proxy(line, proxy_type, username, password))
+                proxies.append(Proxy(line, opts.proxy_type.name, opts.username, opts.password))
                 proxy_stats["suc"] += 1
             else:
                 proxy_stats["fail"] += 1
-            if show_progress:
-                update_proxy_stats(test_proxies)
-    if test_proxies:
-        with ThreadPool(max_workers=proxy_check_threads) as pool:
-            for proxy in proxies:
-                pool.submit(test_proxy, proxy).add_done_callback(proxy_on_checked)
-        proxies = checked
     proxies = list(unique(proxies, key=lambda x: x.__hash__()))
-    if show_progress:
-        print("--- Done checking proxies ---")
+    if opts.show_progress:
         print(f"\nSuccessfully parsed: {proxy_stats['suc'] - proxy_stats['dead']} proxies.")
 
 
-def print_proxy_stats(test_proxies):
+def check_proxies():
+    global proxies, checked, host_ip
+    if __name__ == "__main__":
+        opts.judge = choose_from_enum(Judge, "Please select a proxy judge:")
+    host_ip = get_external_ip()
+    with ThreadPool(max_workers=opts.proxy_check_threads) as pool:
+        for proxy in proxies:
+            pool.submit(test_proxy, proxy).add_done_callback(proxy_on_checked)
+    proxies = checked
+    if opts.show_progress:
+        print()
+
+
+def print_proxy_stats():
     global proxy_stats
     print(
-        f"\nImported: {proxy_stats['suc']} | Dumped: {proxy_stats['fail']} {'| Dead: {}'.format(proxy_stats['dead']) if test_proxies else ''} {'| Alive: {}'.format(len(checked)) if test_proxies else ''} | Running threads: {active_count() - 1} ",
+        f"\nImported: {proxy_stats['suc']} | Dumped: {proxy_stats['fail']} | Dead: {proxy_stats['dead']} | Alive: {proxy_stats['alive']} | Running threads: {active_count() - 1} ",
         end="")
 
 
-def update_proxy_stats(test_proxies):
+def update_proxy_stats():
     global proxy_stats
     print(
-        f"\rImported: {proxy_stats['suc']} | Dumped: {proxy_stats['fail']} {'| Dead: {}'.format(proxy_stats['dead']) if test_proxies else ''}  {'| Alive: {}'.format(len(checked)) if test_proxies else ''} | Running threads: {active_count() - 1} ",
+        f"\rImported: {proxy_stats['suc']} | Dumped: {proxy_stats['fail']} | Dead: {proxy_stats['dead']} | Alive: {proxy_stats['alive']} | Running threads: {active_count() - 1} ",
         end="")
 
 
 def reset_proxies():
-    global proxies, resetting
+    global proxies, resetting, opts
     if resetting:
         while resetting:
             sleep(0.5)
         return
     resetting = True
-    if debug:
+    if opts.debug:
         print("Resetting proxies..")
     for p in proxies:
         p.banned = False
@@ -240,25 +298,28 @@ def reset_proxies():
 
 
 def get_proxy():
-    global proxies
+    global proxies, opts
     i = 0
     while len(proxies[i].workers) >= 5 or proxies[i].dead or proxies[i].banned:
         i += 1
         if i >= len(proxies):
-            if debug:
+            if opts.debug:
                 print("Out of proxies", end="")
             reset_proxies()
             i = 0
-    if debug:
+    if opts.debug:
         print(f"New proxy {proxies[i].proxy}")
     return proxies[i]
 
 
-def open_proxy_file(test_proxies=True, show_progress=True):
-    global proxies
-    print("--------IMPORTANT--------\nRequired proxy format - '0.0.0.0:0', one per line ONLY\n--------IMPORTANT--------")
+def open_proxy_file(options=opts):
+    global proxies, opts
+    opts = options
+    if __name__ == '__main__':
+        print(
+            "--------IMPORTANT--------\nRequired proxy format - '0.0.0.0:0', one per line ONLY\n--------IMPORTANT--------")
     proxies_file = filedialog.askopenfilename(title="Select proxy file.", filetypes=[("Text files", "*.txt")])
-    parse_proxies(proxies_file, show_progress, test_proxies)
+    parse_proxies(proxies_file)
 
 
 if __name__ == '__main__':
